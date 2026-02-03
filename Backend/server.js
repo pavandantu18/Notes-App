2b339f8552044bb8897f3298712bd399f382b708
pavandantu18 <pavandantu18@gmail.com>
2026-02-02 19:34:38 -0500

    Added APIs for notes app

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+/**
+ * Used to start a server
+ * and connect with a database
+ */
+
+require('dotenv').config()
+const app = require("./src/app")
+const connectToDB = require("./src/config/database")
+
+connectToDB()
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
