819d0e96f828864ffb0ba1982039677dbf45fa5a
pavandantu18 <pavandantu18@gmail.com>
2026-02-04 09:18:21 -0500

    added post api feature to frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 const App = () => {
   const [notes, setNotes] = useState([])
 
-  useEffect(() => {
+  function fetchNotes() {
     axios.get('http://localhost:3000/api/notes')
       .then(res => {
         setNotes(res.data.notes)
@@ -12,19 +12,47 @@ const App = () => {
       .catch(err => {
         console.error(err)
       })
-  }, [])
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [notes])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const {title, description} = e.target.elements
+    console.log(title.value,description.value)
+
+    axios.post('http://localhost:3000/api/notes', {
+      title: title.value,
+      description: description.value
+    })
+    .then(res => {
+      console.log(res.data)
+    })
+  }
 
   return (
-    <div className="notes">
-      {
-        notes.map((note, index) => (
-          <div className="note" key={index}>
-            <h1>{note.title}</h1>
-            <p>{note.description}</p>
-          </div>
-        ))
-      }
-    </div>
+    <>
+
+      <form className='note-create-form' onSubmit={handleSubmit}> 
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter description' />
+        <button>Create notes</button>
+      </form>
+
+      <div className="notes">
+        {
+          notes.map((note, index) => (
+            <div className="note" key={index}>
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
